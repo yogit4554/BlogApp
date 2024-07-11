51ac0cccd2a850a9decd2a6483dd947722e37b79
yogit4554 <yt7407286@gmail.com>
2024-07-11 16:29:10 +0530

    added login

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,17 @@
 import Fotter from "./Fotter/Fotter";
 import Header from "./Header/Header";
+import Container from "./container/container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button"
+import Input from "./Input"
 
 export default{
     Header,
-    Fotter
+    Fotter,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input
 }
